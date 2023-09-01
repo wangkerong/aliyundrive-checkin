54c0d8e31a800b53e154d71082fd8b4c3b4f7f54
WangKerong <2585651242@qq.com>
2023-09-01 16:06:43 +0800

    添加psuhplus微信推送，本次代码修改由chatgpt生成

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import fetch from "node-fetch";
-// const notify = require("./sendNotify");
 
 const updateAccesssTokenURL = "https://auth.aliyundrive.com/v2/account/token";
 const signinURL = "https://member.aliyundrive.com/v1/activity/sign_in_list";
@@ -9,68 +8,73 @@ const rewardURL =
 const refreshToeknArry = process.env.REFRESH_TOKENS.split(",").map((i) =>
   i.trim()
 );
+const PUSH_TOKEN = process.env.PUSH_TOKEN
 
-!(async () => {
+async function sendPushNotification(signin_count, rewardInfo) {
+  const pushURL =
+    `http://www.pushplus.plus/send?token=${PUSH_TOKEN}&title=` +
+    `签到成功，本月签到${signin_count}天` +
+    "&content=" +
+    `本次签到获得:${rewardInfo}` +
+    "&template=txt&channel=wechat";
+
+  await fetch(pushURL, { method: "POST" });
+}
+
+(async () => {
   for (const elem of refreshToeknArry) {
     const queryBody = {
       grant_type: "refresh_token",
       refresh_token: elem,
     };
 
-    //使用 refresh_token 更新 access_token
-    fetch(updateAccesssTokenURL, {
-      method: "POST",
-      body: JSON.stringify(queryBody),
-      headers: { "Content-Type": "application/json" },
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        let access_token = json.access_token;
-        // console.log(access_token);
+    try {
+      // 使用 refresh_token 更新 access_token
+      const updateAccessTokenResponse = await fetch(updateAccesssTokenURL, {
+        method: "POST",
+        body: JSON.stringify(queryBody),
+        headers: { "Content-Type": "application/json" },
+      });
+      const updateAccessTokenData = await updateAccessTokenResponse.json();
 
-        //签到
-        fetch(signinURL, {
-          method: "POST",
-          body: JSON.stringify(queryBody),
-          headers: {
-            Authorization: "Bearer " + access_token,
-            "Content-Type": "application/json",
-          },
-        })
-          .then((res) => res.json())
-          .then((json) => {
-            const signin_count = json["result"]["signInCount"];
-            console.log("签到成功, 本月累计签到" + signin_count + "天" + "\n");
+      const access_token = updateAccessTokenData.access_token;
 
-            //领取奖励
-            fetch(rewardURL, {
-              method: "POST",
-              body: JSON.stringify({ signInDay: signin_count }),
-              headers: {
-                Authorization: "Bearer " + access_token,
-                "Content-Type": "application/json",
-              },
-            })
-              .then((res) => res.json())
-              .then((json) => {
-                console.log(
-                  "本次签到获得" +
-                    json["result"]["name"] +
-                    " " +
-                    json["result"]["description"] +
-                    "\n"
-                );
-              })
-              .catch((err) => console.log(err));
-          })
-          .catch((err) => console.log(err));
-      })
-      .catch((err) => console.log(err));
+      // 签到
+      const signinResponse = await fetch(signinURL, {
+        method: "POST",
+        body: JSON.stringify(queryBody),
+        headers: {
+          Authorization: "Bearer " + access_token,
+          "Content-Type": "application/json",
+        },
+      });
+      const signinData = await signinResponse.json();
+
+      const signin_count = signinData.result.signInCount;
+      console.log("签到成功, 本月累计签到" + signin_count + "天" + "\n");
+
+      // 领取奖励
+      const rewardResponse = await fetch(rewardURL, {
+        method: "POST",
+        body: JSON.stringify({ signInDay: signin_count }),
+        headers: {
+          Authorization: "Bearer " + access_token,
+          "Content-Type": "application/json",
+        },
+      });
+      const rewardData = await rewardResponse.json();
+
+      const rewardInfo =
+        rewardData.result.name + " " + rewardData.result.description;
+      console.log("本次签到获得" + rewardInfo + "\n");
+
+      // 发送推送通知
+      await sendPushNotification(signin_count, rewardInfo);
+    } catch (err) {
+      console.log(err);
+    }
   }
-  // await notify.sendNotifyBark(`v2free 自动签到结果`,allnotify)
-})()
-  .catch((e) => {
-    console.error(`❗️  运行错误！\n${e}`);
-  })
-  .finally();
-// notify.sendNotify(`v2free 自动签到结果`,allnotify)
+})().catch((e) => {
+  console.error(`❗️  运行错误！\n${e}`);
+});
+
